f6665aa49a5e3a15fcb5e62d366b112b8cf1bd0b
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-08-14 22:29:41 -0400

    crud practice

---
+++ members.js
@@ -0,0 +1,10 @@
+const members = [
+  {
+    id: 1,
+    name: "test",
+    email: "test@gmail.com",
+  },
+
+];
+
+module.exports = members
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router();
+const members = require('../members')
 
 
 router.get("/", (req,res)=>{
-    res.send("<h1>Hello World<h1>")
+    res.send("<h1>Hello Mandy<h1>")
   
 })
 
@@ -13,13 +14,29 @@ router.get("/about", (req,res)=>{
 })
 
 router.get("/api/:id", (req,res)=>{
-    res.send(req.params.id)
-  
+    for(i=0; i< members.length; i++){
+
+        if(members[i].id === parseInt(req.params.id)){
+            console.log(typeof req.params.id)
+            res.send(members[i])
+           
+        }
+ 
+    }
+    return res.send("members not found");
 })
 
+// router.get("/api/member", (req,res)=>{
+//     res.send(members)
+// })
+
 router.post("/api/member", (req,res)=>{
-    res.send(req.body.food)
-    console.log(req.body.food)
+
+    members.push(req.body)    
+    res.send(members)
+    console.log(members)
 })
+
+
 
 module.exports = router
