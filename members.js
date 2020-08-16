510acaccb10c16982a4aa180c04960f2c442356d
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-08-16 19:26:35 -0400

    crud routes created

--- members.js
+++ members.js
@@ -1,8 +1,18 @@
 const members = [
   {
     id: 1,
-    name: "test",
-    email: "test@gmail.com",
+    name: "test1",
+    email: "test1@gmail.com",
+  },
+  {
+    id: 2,
+    name: "test2",
+    email: "test2@gmail.com",
+  },
+  {
+    id: 3,
+    name: "test3",
+    email: "test3@gmail.com",
   },
 
 ];
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,42 +1,59 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const members = require('../members')
+const members = require("../members");
 
+router.get("/", (req, res) => {
+  res.send("<h1>Hello Mandy<h1>");
+});
 
-router.get("/", (req,res)=>{
-    res.send("<h1>Hello Mandy<h1>")
-  
-})
+router.get("/about", (req, res) => {
+  res.send("<h1>About<h1>");
+});
 
-router.get("/about", (req,res)=>{
-    res.send("<h1>About<h1>")
-  
-})
-
-router.get("/api/:id", (req,res)=>{
-    for(i=0; i< members.length; i++){
-
-        if(members[i].id === parseInt(req.params.id)){
-            console.log(typeof req.params.id)
-            res.send(members[i])
-           
-        }
- 
+// retrieve a member
+router.get("/api/:id", (req, res) => {
+  for (i = 0; i < members.length; i++) {
+    if (members[i].id === parseInt(req.params.id)) {
+      console.log(typeof req.params.id);
+      res.send(members[i]);
     }
-    return res.send("members not found");
-})
+  }
+  return res.send("members not found");
+});
 
 // router.get("/api/member", (req,res)=>{
 //     res.send(members)
 // })
 
-router.post("/api/member", (req,res)=>{
+// add a member
+router.post("/api/member/add", (req, res) => {
+  members.push(req.body);
+  res.send(members);
+  console.log(members);
+});
 
-    members.push(req.body)    
-    res.send(members)
-    console.log(members)
-})
+//delete a member
+router.post("/api/member/delete", (req, res) => {
+  for (i = 0; i < members.length; i++) {
+    if (req.body.id === members[i].id) {
+      members.splice([i], 1);
+      res.send(members);
+    }
+  }
 
+  return res.send("members not found");
+});
 
+//update a member
+router.post("/api/member/update", (req, res) => {
+  for (i = 0; i < members.length; i++) {
+    if (req.body.id === members[i].id) {
+      members[i] = req.body;
+      res.send(members);
+    }
+  }
 
-module.exports = router
+  return res.send("members not found");
+});
+
+module.exports = router;
